b37ef3ce45ba8374cc820b35671f09172f17ee86
breslavsky <breslavsky.anton@gmail.com>
2026-06-14 22:42:48 +0200

    Add select component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
 import { cn } from '../../lib/utils';
 
-function Card({ className, ...props }: React.ComponentProps<'div'>) {
+function Card({
+  asChild = false,
+  className,
+  ...props
+}: React.ComponentProps<'div'> & {
+  asChild?: boolean;
+}) {
+  const Comp = asChild ? Slot : 'div';
+
   return (
-    <div
+    <Comp
       data-slot="card"
       className={cn(
         'bg-card text-card-foreground flex flex-col gap-6 rounded-lg border py-6 shadow-sm',
